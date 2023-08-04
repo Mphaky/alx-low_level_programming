bf2ccbd80f3b1b620075427639839c4d4dc0d3f9
Mphakiseng <beingmphaky@gmail.com>
2023-08-04 00:54:32 -0700

    upated

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integar
+ * @n: the int to check
+ * Rey=turn: the absolute value of int 
+ */
+int -abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
